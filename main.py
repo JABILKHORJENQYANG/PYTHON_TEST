c7e7cc388ccdd5ff328dd31df4093245cdaa4d9a
JenqYang Khor <JenqYang_Khor@jabil.com>
2024-02-01 18:45:15 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+url = "https://prod-191.westus.logic.azure.com:443/workflows/419d7c11a83d4491a42a8c56491cbc0e/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=no_msFm8FWDvTaRwnaZvrI904I3f0LSm0db-6yWNin8"
+data = {
+    "input":"lol"
+}
+headers = {'Content-Type': 'application/json'}
+json_data = json.dumps(data)
+
+response =requests.post(url,headers=headers, data=json_data)
+
+print(response)
