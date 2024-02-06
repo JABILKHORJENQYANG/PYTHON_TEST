53c550198f8ca9c1394e755d07e40d18885b2d51
JenqYang Khor <JenqYang_Khor@jabil.com>
2024-02-06 08:57:23 +0800

    change script

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-import requests
-import json
 import azure.functions as func
 
-app = func.FunctionApp()
-
-@app.function_name(name="HttpTrigger1")
-@app.route(route="req")
-
-def main():
-    
-    url = "https://prod-191.westus.logic.azure.com:443/workflows/419d7c11a83d4491a42a8c56491cbc0e/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=no_msFm8FWDvTaRwnaZvrI904I3f0LSm0db-6yWNin8"
-    data = {
-        "input":"lol"
-    }
-    headers = {'Content-Type': 'application/json'}
-    json_data = json.dumps(data)
-
-    response =requests.post(url,headers=headers, data=json_data)
-
-    print(response)
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    # Your function logic here
+    return func.HttpResponse("Hello from Azure Function App!", status_code=200)
