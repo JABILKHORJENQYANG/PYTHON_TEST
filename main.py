f09f5fc8e072a6f83432c46e2c7ec517a4f1fadd
JenqYang Khor <JenqYang_Khor@jabil.com>
2024-02-06 09:04:26 +0800

    change scriptz

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    # Your function logic here
+    # Your function logic here\
+    print("hello world")
     return func.HttpResponse("Hello from Azure Function App!", status_code=200)
+
